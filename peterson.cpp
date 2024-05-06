dc3ceea94e4addb5a8e5e0a203bdf05eab6c5dfe
Dhruv Laddha <141701231+Dhruvvv-20@users.noreply.github.com>
2024-05-06 09:20:00 +0530

    Update peterson.cpp
    Make improvement

--- peterson.cpp
+++ peterson.cpp
@@ -1,25 +1,65 @@
+
+
 #include <iostream>
+#include <thread>
 using namespace std;
-
-
 int turn = 0;
-int flag[2] = {0,0};
-int process_id = 0;
-
-
-void peterson(int process_id){
-    while(1){
-        flag[process_id] = 1;
-        turn = 1 - process_id;
-        while(turn == 1 - process_id && flag[1-process_id] == 1 );
-        cout << "The process inside critical section is : " << process_id << endl;
-        flag[process_id] = 0;
-    }
+int flag[2] = {0};
+int c = 0, in = 0, out = 0;
+int buffer[10];
+void lock(int p)
+{
+int other = 1 - p;
+flag[p] = 1;
+turn = p;
+while (flag[other] == 1 && turn == p);
 }
-
-int main(){
-    peterson(0);
-    peterson(1);
-    return 0;
+void unlock(int p)
+{
+flag[p] = 0;
 }
-
+void display()
+{
+cout << "The buffer is: ";
+for (int i = 0; i < 10; i++)
+{
+cout << buffer[i] << " ";
+}
+cout << endl;
+}
+void producer()
+{
+for (int i = 0; i < 20; i++)
+{
+while (c == 10);
+lock(0);
+buffer[in] = 100 + i * 2;
+cout << "producer produced: " << buffer[in] << endl;
+in = (in + 1) % 10;
+c++;
+display();
+unlock(0);
+}
+}
+void consumer()
+{
+for (int i = 0; i < 20; i++)
+{
+while (c == 0);
+lock(1);
+int k = buffer[out];
+cout << "Consumer consumed: " << k << endl;
+buffer[out] = 0;
+out = (out + 1) % 10;
+c--;
+display();
+unlock(1);
+}
+}
+int main()
+{
+thread t1(producer);
+thread t2(consumer);
+t1.join();
+t2.join();
+}
